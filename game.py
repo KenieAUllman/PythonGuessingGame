401c08e585de61988ed68956536776f90b8c94fe
Mckena Ullman <ullman.kena@gmail.com>
2022-01-21 09:56:23 -0700

    Get name got t

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
 """A number-guessing game."""
 
 print('Good day, challenger. Do you feel lucky?')
+
+get_name = input("Enter your name, if you dare to continue:")
+
+print("Be prepared to lose,", get_name)
